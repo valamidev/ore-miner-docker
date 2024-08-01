ee1c77a704959ed08a0f756df18f056efdf90b3e
T <52665225+valamidev@users.noreply.github.com>
2024-08-01 08:26:30 +0200

    Update keyGen

--- solana-cli/app/generate-keypair.js
+++ solana-cli/app/generate-keypair.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 // Generate a new keypair
 const keypair = Keypair.generate();
 
-const secretKey = JSON.stringify(Array.from(keypair.secretKey), null);
+const secretKeyString = JSON.stringify(Array.from(keypair.secretKey), null);
 
 if (fs.existsSync("./config/id.json")) {
   console.log("id.json already exists.");
@@ -16,9 +16,9 @@ if (fs.existsSync("./config/id.json")) {
 console.log("Public Key:", keypair.publicKey.toString());
 
 // Print the secret key as an array of numbers
-console.log("Secret Key:", JSON.stringify(Array.from(keypair.secretKey)));
+console.log("Secret Key:", secretKeyString);
 
-fs.writeFileSync("./config/id.json", secretKey);
-fs.writeFileSync("./config/publicKey", keypair.publicKey.toString());
+fs.writeFileSync("./config/id.json", secretKeyString);
+fs.writeFileSync("./config/publicKey", keypair.publicKey.toBase58());
 
 console.log("NEVER SHARE YOUR SECRET KEY or your id.json file!");
